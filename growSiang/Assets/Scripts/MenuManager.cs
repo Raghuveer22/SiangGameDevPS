f38f608292d65a886cfa21cb8cff08e2ba2649e3
Raghuveer22 <posa.mokshith@iitg.ac.in>
2023-02-15 20:28:13 +0530

    added mainMenu

---
+++ growSiang/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public static MenuManager Instance; 
+    [SerializeField] Menu[] menus;
+     void Awake()
+    {
+        Instance = this;
+    }
+    public void OpenMenu(string menuName)
+    {
+       
+        for (int i = 0; i < menus.Length; i++)
+        {
+            if (menus[i].menuName == menuName)
+            {
+                menus[i].Open();
+            }
+            else if (menus[i].open)
+            {
+
+                CloseMenu(menus[i]);
+            }
+        }
+    }
+
+    public void OpenMenu(Menu menu)
+    {
+        for (int i = 0; i < menus.Length; i++)
+        {
+            if (menus[i].open)
+            {
+                CloseMenu(menus[i]);
+            }
+        }
+        menu.Open();
+    }
+    public void CloseMenu(Menu menu)
+    {
+        menu.Close();
+    }
+    public void Exit()
+    {
+            #if UNITY_EDITOR
+               UnityEditor.EditorApplication.isPlaying = false;
+            #endif
+        Application.Quit();
+
+    }
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
