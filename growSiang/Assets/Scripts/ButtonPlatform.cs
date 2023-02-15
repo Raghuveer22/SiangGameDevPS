084a02b34b59bf7a1834d17cbde7a46bdb8dd5b2
Raghuveer22 <posa.mokshith@iitg.ac.in>
2023-02-15 23:42:41 +0530

    prefabs finished

---
+++ growSiang/Assets/Scripts/ButtonPlatform.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class ButtonPlatform : MonoBehaviour
+{
+    [SerializeField] private GameObject[] wayPoints;
+    [SerializeField] public int currentWayPointIndex = 0;
+    [SerializeField] private float speed = 2f;
+    void Update()
+    {
+       
+        transform.position = Vector2.MoveTowards(transform.position, wayPoints[currentWayPointIndex].transform.position, Time.deltaTime * speed);
+
+
+    }
+    public void SetState(int t)
+    {
+        currentWayPointIndex = t;
+    }
+}
---
+++ growSiang/Assets/Scripts/ButtonSystem.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+public class ButtonSystem : MonoBehaviour
+{
+    [SerializeField] private GameObject MovingPlatform;
+    private int index = 0;
+    Color hitted = new Color32(19, 255, 246,255);
+    Color unhitted = new Color32(253,0, 0,255);
+    // Start is called before the first frame update
+    void Start()
+    {
+        MovingPlatform = transform.parent.GetChild(0).gameObject;
+        MovingPlatform = MovingPlatform.transform.GetChild(1).gameObject;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+     
+        if(index==0)
+        {
+            index = 1;
+            gameObject.GetComponent<SpriteRenderer?>().color = hitted;
+        }
+        else if(index==1)
+        {
+            index = 0;
+            gameObject.GetComponent<SpriteRenderer?>().color = unhitted;
+        }
+        if (MovingPlatform?.GetComponent<ButtonPlatform?>().currentWayPointIndex != null)
+        {
+            MovingPlatform?.GetComponent<ButtonPlatform?>().SetState(index);
+        }
+
+
+    }
+
+  
+}
